09f356f1ccfe6a06033adde6f3c672f3f9f13614
ADL175 <armydavidlim@gmail.com>
2017-09-03 15:55:23 -0700

    feature branch

--- src/com/example/java/Calculator.java
+++ src/com/example/java/Calculator.java
@@ -3,7 +3,7 @@ package com.example.java;
 import java.util.Scanner;
 
 import static com.example.java.util.MathMethods.*;
-//some comments again again
+//feature stuff
 public class Calculator {
 
     public static void main(String[] args) {
