111dc8a6b58e61fbe0f60b64ff1172cc907665c6
ADL175 <armydavidlim@gmail.com>
2017-09-03 15:52:18 -0700

    Some changes

--- src/com/example/java/Calculator.java
+++ src/com/example/java/Calculator.java
@@ -3,7 +3,7 @@ package com.example.java;
 import java.util.Scanner;
 
 import static com.example.java.util.MathMethods.*;
-
+//some comments
 public class Calculator {
 
     public static void main(String[] args) {
